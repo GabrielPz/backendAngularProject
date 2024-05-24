3d7bc7278967fa5cfd9f9175a2869c5799e4df26
Gabriel <gabrielhenriquedps@gmail.com>
2024-05-24 18:32:53 -0300

    feat: add mocked admin user

--- src/controllers/api-users.ts
+++ src/controllers/api-users.ts
@@ -5,7 +5,9 @@ import { prisma } from "../lib/prisma";
 export const login = async (req: Request, res: Response) => {
   try {
     const { username, password } = req.query;
-
+    if(username === 'admin' && password === "admin"){
+      return res.status(200).send({ message: "Login successful.", user: username });
+    }
     if (!username || !password) {
       return res.status(400).send({ message: "Username and password are required." });
     }
